4d3139bcad2ce30e59673dedf38a4f7ec6c31afd
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-11-20 15:24:45 +0700

    Update Homework1_1.cpp

--- Homework/Homework1_1.cpp
+++ Homework/Homework1_1.cpp
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 int main() {
-    int N, i;
+    int N, i ;
     
     printf( "Enter Number: " ) ;
     
     // รับค่า N (จำนวนรอบ)
-    if (scanf("%d", &N) != 1) {
-        return 1; // Handle input failure
+    if( scanf ( "%d", &N ) != 1 ) {
+        return 1 ; // Handle input failure
     } 
     
-    if ( N < 1 ) {
+    if( N < 1 ) {
         printf( "Error" ) ;
     } else {
         // TODO: Implement the for loop to print the message N times
-        for (i = 0; i < N; i++)
-         printf("Hello Loop!\n"); 
+        for( i = 0 ; i < N ; i++ )
+         printf( "Hello Loop!\n" ) ; 
     }
 
-    return 0;
+    return 0 ;
 }
