b3cafe2ab9ca6a651c3f89df97fc43884416878b
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-11-20 15:10:18 +0700

    Update Homework1_1.cpp

--- Homework/Homework1_1.cpp
+++ Homework/Homework1_1.cpp
@@ -1,1 +1,23 @@
+// Pre-code (โค้ดบังคับ)
+#include <stdio.h>
 
+int main() {
+    int N, i;
+    
+    printf( "Enter Number: " ) ;
+    
+    // รับค่า N (จำนวนรอบ)
+    if (scanf("%d", &N) != 1) {
+        return 1; // Handle input failure
+    } 
+    
+    if ( N < 1 ) {
+        printf( "Error" ) ;
+    } else {
+        // TODO: Implement the for loop to print the message N times
+        for (i = 0; i < N; i++)
+         printf("Hello Loop!\n"); 
+    }
+
+    return 0;
+}
