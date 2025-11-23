0594d9c26e6b93dceca393cf22c02fa1f2b7dab9
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-11-23 20:29:47 +0700

    Improve score input and grading output
    Refactor grading logic and input handling in main function.

--- Homework/1_2.cpp
+++ Homework/1_2.cpp
@@ -1,24 +1,48 @@
-// Pre-code (โค้ดบังคับ)
 #include <stdio.h>
 
 int main() {
     int score ;
-
-    printf( "Enter your score " ) ;
-    if (scanf( "%d", &score ) != 1) {
-        return 1 ; 
+    
+    if (scanf ( "%d" , &score ) != 1 ) {
+        return 1 ;
     }
-   
-    if ( score >= 80 ) {
-        printf( "Grad A\n" ) ;
-    } else if ( score >= 70 ) { 
-        printf( "Grad B\n" ) ;
-    } else if ( score >= 60 ) { 
-        printf( "Grad C\n" ) ;
-    } else if ( score >= 50 ) {
-        printf( "Grad D\n" ) ;
-    } else { 
-        printf( "Grad F\n" ) ;
+    
+    if( score <= 49 ) {
+        
+        printf( "Grade F" ) ;
+        
+    }else if ( score >= 50 && score <= 54 ) {
+        
+        printf( "Grade D") ;
+        
+    }else if ( score >= 55 && score <= 59 ) {
+        
+        printf( "Grade D+") ;
+        
+    }else if ( score >= 60 && score <= 64 ) {
+        
+        printf( "Grade C") ;
+        
+    }else if ( score >= 65 && score <= 69 ) {
+        
+        printf( "Grade C+") ;
+        
+    }else if ( score >= 70 && score <= 74 ) {
+        
+        printf( "Grade B") ;
+        
+    }else if ( score >= 75 && score <= 79 ) {
+        
+        printf( "Grade B+") ;
+        
+    }else if ( score >= 80 ) {
+        
+        printf( "Grade A") ;
+        
+    }else {
+        
+        printf( "Error !!!" ) ;
     }
+    
     return 0 ;
 }
