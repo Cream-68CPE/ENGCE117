078b9a0d6cc33f3dcc084f0de38d27e10d36d93e
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-11-23 20:54:06 +0700

    Update 1_3.cpp

--- Homework/1_3.cpp
+++ Homework/1_3.cpp
@@ -1,45 +1,38 @@
-// Pre-code (โค้ดบังคับ)
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int num1, num2, operationCode, result ;
     
-    // รับค่าตัวเลข 2 ตัวและรหัสคำสั่ง
-    printf( "Enter two Number (num1, num2) and the operation code (1-4):\n" ) ;
-    if (scanf( "%d %d %d", &num1, &num2, &operationCode ) != 3) {
-        printf( "Input error.\n" ) ;
-        return 1 ; 
+    if (scanf ( "%d %d %d" , &num1, &num2, &operationCode) != 3 ) {
+        
+        return 1 ;
+        
     }
     
-    switch (operationCode) {
-        case 1: // บวก 
-            result = num1 + num2 ;
-            printf( "Result: %d + %d = %d\n", num1, num2, result ) ;
-            break ;
+    switch( operationCode ) {
+        
+        case 1 :
+            printf( "%d" , num1 + num2 ) ;
+        break ;
+        
+        
+        case 2 :
+            printf( "%d" , num1 - num2 ) ;
+        break ;
+        
+        case 3 :
+            printf( "%d" , num1 * num2 ) ;
+        break ;
+        
+        case 4 :
+            printf( "%d" , num1 / num2 ) ;
+        break ;
             
-        case 2: // ลบ 
-            result = num1 - num2;
-            printf( "Result: %d - %d = %d\n", num1, num2, result ) ;
-            break ;
-            
-        case 3: // คูณ 
-            result = num1 * num2 ;
-            printf( "Result: %d * %d = %d\n", num1, num2, result ) ;
-            break ;
-            
-        case 4: // หาร 
-            if (num2 == 0) {
-                printf("Error: Division by zero is not allowed.\n") ;
-                return 1 ; 
-            }
-            result = num1 / num2;
-            printf("Result: %d / %d = %d\n", num1, num2, result) ;
-            break ;
-            
-        default: 
-            printf( "Invalid Operation\n" ) ;
-            break ;
+        default :
+            printf( "Invalid Operation" ) ;
+        
     }
-
+    
     return 0 ;
 }
