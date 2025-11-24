53f4b24029da28abf868f7e1c60a2c76f6c14ef2
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-11-24 19:09:09 +0700

    Update 1_8.cpp

--- Homework/1_8.cpp
+++ Homework/1_8.cpp
@@ -1,1 +1,27 @@
+#include <stdio.h>
+int main() {
+    int dayCode, hour ;
 
+    printf( "Enter Day Code (1-7) and Hour (0-23):") ;
+
+    scanf("%d %d", &dayCode, &hour ) ;
+
+    switch (dayCode) {
+    case 1: case 2: case 3: case 4: case 5:
+    if ( hour>=8 && hour<=17 ) { 
+        printf ( "System Running (Workday)\n" ) ;
+      } else {
+           printf ( "System Idle (Off-hours)\n" ) ;
+      }
+        break ;
+
+    case 6: case 7: 
+        printf ( "Weekend Relax Mode\n" ) ;
+        break;
+
+    default:
+        printf( "Invalid Day Code\n" ) ;
+    }
+
+    return 0 ;
+}
