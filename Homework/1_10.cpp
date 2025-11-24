6bb8df8282ef376dba1325ec3cfb3c7e3674f689
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-11-24 20:39:59 +0700

    Implement access control based on user input
    Added a program to check access based on clearance level, age, and activity status.

--- Homework/1_10.cpp
+++ Homework/1_10.cpp
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+    int clearanceLevel, age, isActive;
+
+    printf( "Enter clearanceLevel , Ago and IsActive: 1=Active, 0=Inactive :" ) ;
+
+    if (scanf("%d %d %d", &clearanceLevel, &age, &isActive) != 3) {
+        return 1;
+    }
+    
+    if ( (clearanceLevel == 3 && isActive == 1) || 
+         (clearanceLevel == 2 && age >= 25 && isActive == 1) ) {
+        
+        printf("Access Granted\n");
+        
+    } else {
+        
+        printf("Access Denied\n");
+        
+    }
+
+    return 0;
+}
