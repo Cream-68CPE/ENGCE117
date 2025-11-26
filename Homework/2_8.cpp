a6159811cdf64c875ad355749229820f2947ca1f
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-11-26 20:32:37 +0700

    Create 2_8.cpp

---
+++ Homework/2_8.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int N, i ;
+    int studentId, score, totalClasses, absentClasses ;
+    float attendancePercent ;
+    int passCount = 0 ;
+    int failCount = 0 ;
+
+ printf( " Enter N (Number of students):") ;
+ printf ("studentld numberb, score, totalClass and adsentcalls:") ;
+    if ( scanf( "%d", &N ) != 1 ) {
+        return 1 ;
+    }
+
+    for ( i = 0; i < N; i++ ) {
+
+        if ( scanf( "%d %d %d %d", &studentId, &score, &totalClasses, &absentClasses ) != 4 ) {
+            break ;
+        }
+        attendancePercent = (float)(totalClasses - absentClasses) / totalClasses * 100.0 ;
+
+        if ( score >= 50 && attendancePercent >= 75.0 ) {
+            printf( "Student ID %d: PASS\n", studentId ) ;
+            passCount++ ;
+        } 
+        else {
+            failCount++ ;
+            printf( "Student ID %d: FAIL -> Reason: ", studentId ) ;
+
+            if ( score < 50 && attendancePercent < 75.0 ) {
+                printf( "Both Score and Attendance\n" ) ;
+            } 
+            else if ( score < 50 ) {
+                printf( "Score\n" ) ;
+            } 
+            else {
+                printf( "Attendance\n" ) ;
+            }
+        }
+    }
+    printf( "\n--- Summary ---\n" ) ;
+    printf( "Total PASS: %d\n", passCount ) ;
+    printf( "Total FAIL: %d\n", failCount ) ;
+
+    return 0 ;
+}
