662ef1792eef6126735eeb690294d2682a940950
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-11-26 15:08:57 +0700

    Update 2_2.cpp

--- Homework/2_2.cpp
+++ Homework/2_2.cpp
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main()
+{
+    int N, i, quantity ;
+    float unitPrice, itemCost, grandTotal = 0.0 ;
+
+    if ( scanf( "%d", &N ) != 1 ) {
+        return 1 ;
+    }
+
+    for ( i = 0 ; i < N ; i++ ) {
+
+        scanf( "%f %d", &unitPrice, &quantity ) ;
+
+        if ( unitPrice >= 1000.00 ) {
+            
+            itemCost = ( unitPrice * quantity ) * 0.90 ;
+
+        } else {
+
+            itemCost = unitPrice * quantity ;
+
+        }
+
+        grandTotal += itemCost ;
+
+    }
+   
+    printf( "Grand Total: %.2f\n", grandTotal ) ;
+    return 0 ;
+}
