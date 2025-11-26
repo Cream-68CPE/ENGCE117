9c13b065264a716ce94aa375fa9269219f53d407
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-11-26 18:41:20 +0700

    Update 2_5.cpp

--- Homework/2_5.cpp
+++ Homework/2_5.cpp
@@ -1,1 +1,41 @@
+#include <stdio.h>
 
+int main() {
+    int score ;
+    int countA = 0 ;
+    int countB = 0 ;
+    int countC = 0 ;
+    int countD = 0 ;
+    int countF = 0 ;
+
+    printf( "Enter score :") ;
+    if ( scanf( "%d", &score ) != 1 ) {
+        return 1 ;
+    }
+
+    while ( score != -1 ) {
+
+        if ( score >= 80 ) {
+            countA++ ;
+        } else if ( score >= 70 ) {
+            countB++ ;
+        } else if ( score >= 60 ) {
+            countC++ ;
+        } else if ( score >= 50 ) {
+            countD++ ;
+        } else {
+            countF++ ;
+        }
+
+        if ( scanf( "%d", &score ) != 1 ) {
+            break ;
+        }
+    }
+    printf( "Grade A Count: %d\n", countA ) ;
+    printf( "Grade B Count: %d\n", countB ) ;
+    printf( "Grade C Count: %d\n", countC ) ;
+    printf( "Grade D Count: %d\n", countD ) ;
+    printf( "Grade F Count: %d\n", countF ) ;
+
+    return 0 ;
+}
