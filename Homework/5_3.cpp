cb985fdb74f2b6b9559576020ec2e2bf2108c35a
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-11-30 00:49:57 +0700

    Update 5_3.cpp

--- Homework/5_3.cpp
+++ Homework/5_3.cpp
@@ -1,1 +1,45 @@
+#include <stdio.h>
 
+#define SIZE 5
+
+int calculate_sum( int array[], int size );
+
+int main() 
+{
+    int numbers[ SIZE ];
+    int total_sum ;
+    float average ;
+    int i ;
+
+    for ( i = 0; i < SIZE; i++ ) {
+        scanf( "%d", &numbers[ i ] );
+    }
+
+    total_sum = calculate_sum( numbers, SIZE );
+    average = ( float ) total_sum / SIZE ;
+
+    printf( "--- ARRAY AVERAGE REPORT ---\n" );
+    printf( "Recorded Numbers: " );
+    
+    for ( i = 0; i < SIZE; i++ ) {
+        printf( "%d ", numbers[ i ] );
+    }
+    
+    printf( "\n" );
+    printf( "Total Sum: %d\n", total_sum );
+    printf( "Average: %.2f\n", average );
+
+    return 0;
+}
+
+int calculate_sum( int array[], int size ) 
+{
+    int sum = 0 ;
+    int i ;
+    
+    for ( i = 0; i < size; i++ ) {
+        sum += array[ i ] ;
+    }
+    
+    return sum ;
+}
