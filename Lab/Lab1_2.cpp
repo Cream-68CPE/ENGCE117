b66fcb0aa015e8ba465023195d90af13be16e466
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-12-03 12:57:24 +0700

    Update Lab1_2.cpp

--- Lab/Lab1_2.cpp
+++ Lab/Lab1_2.cpp
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+void GetSet( int [] , int * ) ;
+
+int main() {
+    
+    int *data, num ;
+    
+    GetSet( (int *)&data , &num ) ;
+    
+    return 0 ;
+    
+}
+
+void GetSet( int set[] , int *num ) {
+    
+    static int buffer[ 100 ] ;
+    
+    int **ptrToData = ( int ** ) set ;
+    
+    *ptrToData = buffer ;
+    
+    scanf( "%d" , num ) ;
+    
+    int i = 0 ;
+    for ( i = 0 ; i < *num ; i++ ) {
+        
+        scanf( "%d" , &buffer[ i ] ) ;
+        
+    }
+    
+}
