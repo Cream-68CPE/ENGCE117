d0a0813e037476ae031183bd435b51d6edaac945
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-12-05 12:21:40 +0700

    Update Lab1_4.cpp

--- Lab/Lab1_4.cpp
+++ Lab/Lab1_4.cpp
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int temp[100];
+
+int GetSet(int *array[]) {
+    int i, n;
+
+    printf("Enter number of elements: ");
+    if (scanf("%d", &n) != 1) n = 0;
+
+    if (n < 0) n = 0;
+    if (n > 100) n = 100;
+
+    for (i = 0; i < n; i++) {
+        printf("Element %d: ", i + 1);
+        if (scanf("%d", &temp[i]) != 1) temp[i] = 0;
+    }
+
+    *array = temp;
+    return n;
+}
+
+int main() {
+    int *data, num, i;
+    num = GetSet(&data);
+
+    printf("\nSet elements: ");
+    for (i = 0; i < num; i++) {
+        printf("%d ", data[i]);
+    }
+
+    return 0;
+}
