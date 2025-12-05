c40bc3f975dc60b73f5f24c844c974b8fd90b029
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-12-05 12:42:09 +0700

    Update Lab1_6.cpp

--- Lab/Lab1_6.cpp
+++ Lab/Lab1_6.cpp
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int *GetMatrix(int *numRows, int *numCols) {
+    int size;
+    int *matrix;
+
+    scanf("%d %d", numRows, numCols);
+
+    size = (*numRows) * (*numCols);
+    matrix = (int *)malloc(size * sizeof(int));
+
+    if (matrix == NULL) {
+        exit(0);
+    }
+
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &matrix[i]);
+    }
+
+    return matrix;
+}
+
+int main() {
+    int *matrixData;
+    int m, n;
+
+    matrixData = GetMatrix(&m, &n);
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrixData[i * n + j]);
+        }
+        printf("\n");
+    }
+
+    free(matrixData);
+    return 0;
+}
