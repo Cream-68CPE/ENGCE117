5bda99320e7752570ca31e35c5fb8067b624fd94
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-12-06 00:33:34 +0700

    Update Lab2_2.cpp

--- Lab/Lab2_2.cpp
+++ Lab/Lab2_2.cpp
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <string.h>
 
+char* reverse( char str1[] ) ;
+
+int main() {
+    char text[ 50 ] = "I Love You" ;
+    char *out ;
+    
+    out = reverse( text ) ;
+    
+    printf( "%s\n", out ) ;
+    
+    return 0 ;
+}
+
+char* reverse( char str1[] ) {
+    int len = strlen( str1 ) ;
+    int i ;
+    char temp ;
+    
+    for ( i = 0 ; i < len / 2 ; i++ ) {
+        temp = str1[ i ] ;
+        str1[ i ] = str1[ len - 1 - i ] ;
+        str1[ len - 1 - i ] = temp ;
+    }
+    
+    return str1 ;
+}
