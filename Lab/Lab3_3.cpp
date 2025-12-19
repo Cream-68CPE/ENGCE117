0da95139d51c5785c667294e1960023ad1198dff
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-12-19 13:11:37 +0700

    Implement student struct and GPA upgrade functionality
    Added a student struct and implemented GPA upgrade logic based on sex.

--- Lab/Lab3_3.cpp
+++ Lab/Lab3_3.cpp
@@ -1,1 +1,48 @@
+#include <stdio.h>
 
+struct student {
+    char name[ 20 ] ;
+    int age ;
+    char sex ;
+    float gpa ;
+} ;
+
+struct student upgrade( struct student child ) ;
+
+int main() {
+    
+    struct student aboy ;
+    
+    // --- INPUT ---
+    aboy.sex = 'B' ;
+    aboy.gpa = 3.00 ;
+    
+    // --- PROCESS ---
+    aboy = upgrade( aboy ) ;
+    
+    // --- OUTPUT ---
+    printf( "GPA in main : %.2f\n", aboy.gpa ) ;
+    
+    return 0 ;
+    
+}
+
+struct student upgrade( struct student child ) {
+    
+    // --- PROCESS ---
+    if ( child.sex == 'B' ) {
+        
+        child.gpa = child.gpa * 1.10 ;
+        
+    } else if ( child.sex == 'G' ) {
+        
+        child.gpa = child.gpa * 1.20 ;
+        
+    }
+    
+    // --- OUTPUT (Local) ---
+    printf( "GPA [%c] inside function : %.2f\n", child.sex, child.gpa ) ;
+    
+    return child ;
+    
+}
