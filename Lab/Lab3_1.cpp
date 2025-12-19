ffdb4b88f5e46e39539da6784964a83094b5f4f4
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-12-19 13:10:37 +0700

    Update Lab3_1.cpp

--- Lab/Lab3_1.cpp
+++ Lab/Lab3_1.cpp
@@ -1,1 +1,48 @@
+#include <stdio.h>
 
+struct student {
+    char name[ 20 ] ;
+    int age ;
+    char sex ;
+    float gpa ;
+} ;
+
+void upgrade( struct student child ) ;
+
+int main() {
+    
+    struct student aboy ;
+    
+    // --- INPUT ---
+    aboy.sex = 'G' ;
+    aboy.gpa = 3.00 ;
+    
+    // --- PROCESS ---
+    upgrade( aboy ) ;
+    
+    // --- OUTPUT ---
+    printf( "GPA in main : %.2f\n", aboy.gpa ) ;
+    
+    return 0 ;
+    
+}
+
+void upgrade( struct student child ) {
+    
+    // --- INPUT (Local) ---
+
+    // --- PROCESS ---
+    if ( child.sex == 'B' ) {
+        
+        child.gpa = child.gpa * 1.10 ;
+        
+    } else if ( child.sex == 'G' ) {
+        
+        child.gpa = child.gpa * 1.20 ;
+        
+    }
+    
+    // --- OUTPUT ---
+    printf( "GPA [%c]: %.2f\n", child.sex, child.gpa ) ;
+    
+}
