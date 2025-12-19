85cb9055133567a023f156d257918c4d785ffb3f
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-12-19 13:11:04 +0700

    Implement student GPA upgrade based on sex

--- Lab/Lab3_2.cpp
+++ Lab/Lab3_2.cpp
@@ -1,1 +1,46 @@
+#include <stdio.h>
 
+struct student {
+    char name[ 20 ] ;
+    int age ;
+    char sex ; 
+    float gpa ;
+} ;
+
+void upgrade( struct student *child ) ;
+
+int main() {
+    
+    struct student aboy ;
+    
+    // --- INPUT ---
+    aboy.sex = 'B' ; 
+    aboy.gpa = 3.00 ;
+    
+    // --- PROCESS ---
+    upgrade( &aboy ) ; 
+    
+    // --- OUTPUT ---
+    printf( "GPA in main : %.2f\n", aboy.gpa ) ;
+    
+    return 0 ;
+    
+}
+
+void upgrade( struct student *child ) {
+    
+    // --- PROCESS ---
+    if ( child->sex == 'B' ) {
+        
+        child->gpa = child->gpa * 1.10 ;
+        
+    } else if ( child->sex == 'G' ) {
+        
+        child->gpa = child->gpa * 1.20 ;
+        
+    }
+    
+    // --- OUTPUT ---
+    printf( "GPA [%c] : %.2f\n", child->sex, child->gpa ) ;
+    
+}
