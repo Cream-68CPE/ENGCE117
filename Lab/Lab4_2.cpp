ed3f358dea949f9fdda56ad43bb2184d2299b21f
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2025-12-19 13:13:31 +0700

    Implement pointer manipulation in Lab4_2.cpp

--- Lab/Lab4_2.cpp
+++ Lab/Lab4_2.cpp
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+void go( int ***p, int **z ) ;
+
+int main() {
+    
+    int *b = new int ; *b = 10 ;
+    int *c = new int ; *c = 20 ;
+    int **a ;
+    
+    // --- PROCESS 1 ---
+    go( &a, &b ) ;
+    
+    // --- OUTPUT 1 ---
+    printf( "%d %p %p %p\n", **a, *a, a, &a ) ;
+    
+    // --- PROCESS 2 ---
+    go( &a, &c ) ;
+    
+    // --- OUTPUT 2 ---
+    printf( "%d %p %p %p\n", **a, *a, a, &a ) ;
+    
+    return 0 ;
+    
+}
+
+void go( int ***p, int **z ) {
+    *p = z ; 
+}
