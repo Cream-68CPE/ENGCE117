e60262e80ec9ffa2d6c1ea3a7de3d690b775ac0d
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2026-02-25 20:46:08 +0700

    Add KnapsackGreedy function for solving knapsack
    Implement greedy algorithm for knapsack problem.

--- Lab/12_2.cpp
+++ Lab/12_2.cpp
@@ -1,1 +1,41 @@
+int *KnapsackGreedy( int *w, int *v, int n, int wx ) {
+    static int x[ 5 ] ; 
+    int i, j, best_idx ;
+    float max_ratio, ratio ;
+    
+    for ( i = 0 ; i < n ; i++ ) {
+        x[ i ] = 0 ;
+    }
 
+    for ( i = 0 ; i < n ; i++ ) {
+        max_ratio = -1.0 ;
+        best_idx = -1 ;
+
+        for ( j = 0 ; j < n ; j++ ) {
+            if ( x[ j ] == 0 ) {
+                ratio = ( float ) v[ j ] / w[ j ] ;
+                if ( ratio > max_ratio ) {
+                    max_ratio = ratio ;
+                    best_idx = j ;
+                }
+            }
+        }
+
+        if ( best_idx != -1 ) {
+            if ( w[ best_idx ] <= wx ) {
+                x[ best_idx ] = 1 ;
+                wx -= w[ best_idx ] ;
+            } else {
+                x[ best_idx ] = -1 ; 
+            }
+        }
+    }
+
+    for ( i = 0 ; i < n ; i++ ) {
+        if ( x[ i ] == -1 ) {
+            x[ i ] = 0 ;
+        }
+    }
+
+    return x ;
+}
