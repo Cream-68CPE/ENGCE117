892d46955ff43c1bd6141d181010b54ee3ca0dfc
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2026-02-25 11:43:16 +0700

    Implement Tower of Hanoi algorithm in Lab10_2.cpp

--- Lab/Lab10_2.cpp
+++ Lab/Lab10_2.cpp
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+void TowerHanoi( int m, int i, int j ) ;
+
+int main()
+{
+    TowerHanoi( 3, 1, 3 ) ;
+    
+    return 0 ;
+}
+
+void TowerHanoi( int m, int i, int j )
+{
+    int k ;
+    
+    if ( m > 0 ) {
+        
+        k = 6 - i - j ;
+        
+        TowerHanoi( m - 1, i, k ) ;
+        
+        printf( "Disc %d from %d to %d\n", m, i, j ) ;
+        
+        TowerHanoi( m - 1, k, j ) ;
+        
+    }
+}
