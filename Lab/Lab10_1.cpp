74a5b0b22866717b7ff1622565a6287099af42d4
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2026-02-25 11:42:49 +0700

    Update Lab10_1.cpp

--- Lab/Lab10_1.cpp
+++ Lab/Lab10_1.cpp
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+long SumLoop( int n ) ;
+long SumRecur( int n ) ;
+
+int main()
+{
+    printf( "SumLoop(n) = %d\n", SumLoop( 10 ) ) ;
+    printf( "SumRecur(n) = %d\n", SumRecur( 10 ) ) ;
+    
+    return 0 ;
+}
+
+long SumLoop( int n )
+{
+    long sum = 0 ;
+    int i ;
+    
+    for ( i = 1 ; i <= n ; i++ ) {
+        
+        sum += i ;
+        
+    }
+    
+    return sum ;
+}
+
+long SumRecur( int n )
+{
+    if ( n <= 0 ) {
+        
+        return 0 ;
+        
+    }
+    
+    return n + SumRecur( n - 1 ) ;
+}
