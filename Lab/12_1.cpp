8baa25dca1e2305a275c08f08bf7e0d84b20ea18
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2026-02-25 20:50:13 +0700

    Update 12_1.cpp

--- Lab/12_1.cpp
+++ Lab/12_1.cpp
@@ -1,23 +1,20 @@
-#include <stdio.h>
-#include <limits.h>
-
 int *Dijkstra( int *L, int n ) {
-    static int dist[ 5 ] ;
+    static int d[ 5 ] ;
     int visited[ 5 ] = { 0, 0, 0, 0, 0 } ;
-    int i, count, u, v ;
+    int i, j, u, v, min ;
 
     for ( i = 0 ; i < n ; i++ ) {
-        dist[ i ] = 999999 ;
+        d[ i ] = 9999 ;
     }
 
-    dist[ 0 ] = 0 ;
+    d[ 0 ] = 0 ;
 
-    for ( count = 0 ; count < n ; count++ ) {
-        int min = 999999 ;
-        for ( i = 0 ; i < n ; i++ ) {
-            if ( !visited[ i ] && dist[ i ] < min ) {
-                min = dist[ i ] ;
-                u = i ;
+    for ( i = 0 ; i < n ; i++ ) {
+        min = 9999 ;
+        for ( j = 0 ; j < n ; j++ ) {
+            if ( !visited[ j ] && d[ j ] < min ) {
+                min = d[ j ] ;
+                u = j ;
             }
         }
 
@@ -26,18 +23,18 @@ int *Dijkstra( int *L, int n ) {
         for ( v = 0 ; v < n ; v++ ) {
             int weight = L[ u * n + v ] ;
             if ( !visited[ v ] && weight != -1 ) {
-                if ( dist[ u ] + weight < dist[ v ] ) {
-                    dist[ v ] = dist[ u ] + weight ;
+                if ( d[ u ] + weight < d[ v ] ) {
+                    d[ v ] = d[ u ] + weight ;
                 }
             }
         }
     }
 
     static int res[ 4 ] ;
-    res[ 0 ] = dist[ 3 ] + 20 ; 
-    res[ 1 ] = dist[ 2 ] - 20 ; 
-    res[ 2 ] = dist[ 3 ] ;      
-    res[ 3 ] = dist[ 4 ] ;      
+    res[ 0 ] = d[ 1 ] ;
+    res[ 1 ] = d[ 2 ] ;
+    res[ 2 ] = d[ 3 ] ;
+    res[ 3 ] = d[ 4 ] ;
 
     return res ;
 }
