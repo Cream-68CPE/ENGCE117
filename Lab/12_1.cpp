64b8bb1d77dffd21767c02ffa362b1bbcc66081e
Yanisa thippanya(CREAM) <yanisacream08@gmail.com>
2026-02-25 23:51:21 +0700

    Update 12_1.cpp

--- Lab/12_1.cpp
+++ Lab/12_1.cpp
@@ -1,40 +1,83 @@
-int *Dijkstra( int *L, int n ) {
-    static int d[ 5 ] ;
-    int visited[ 5 ] = { 0, 0, 0, 0, 0 } ;
-    int i, j, u, v, min ;
+#include <stdio.h>
+#include <stdlib.h>
 
+#define INF 9999
+#define NO_EDGE -1
+
+int getMinVertex( int *d, int *visited, int n ) {
+    int min = INF ;
+    int u = -1 ;
+    int j ;
+    
+    for ( j = 0 ; j < n ; j++ ) {
+        if ( !visited[ j ] && d[ j ] < min ) {
+            min = d[ j ] ;
+            u = j ;
+        }
+    }
+    
+    return u ;
+}
+
+int *Dijkstra( int *L, int n ) {
+    
+    int *d = ( int * ) malloc( n * sizeof( int ) ) ;
+    int *visited = ( int * ) calloc( n, sizeof( int ) ) ;
+    int i, u, v ;
+
+   
     for ( i = 0 ; i < n ; i++ ) {
-        d[ i ] = 9999 ;
+        d[ i ] = INF ;
     }
 
-    d[ 0 ] = 0 ;
-
+    d[ 0 ] = 0 ; 
     for ( i = 0 ; i < n ; i++ ) {
-        min = 9999 ;
-        for ( j = 0 ; j < n ; j++ ) {
-            if ( !visited[ j ] && d[ j ] < min ) {
-                min = d[ j ] ;
-                u = j ;
-            }
+        
+        u = getMinVertex( d, visited, n ) ;
+        
+        if ( u == -1 ) {
+            break ; 
         }
-
+        
         visited[ u ] = 1 ;
 
         for ( v = 0 ; v < n ; v++ ) {
+            
             int weight = L[ u * n + v ] ;
-            if ( !visited[ v ] && weight != -1 ) {
-                if ( d[ u ] + weight < d[ v ] ) {
-                    d[ v ] = d[ u ] + weight ;
-                }
+            
+            /* รวมเงื่อนไข if เพื่อลดความลึก (Nesting) */
+            if ( !visited[ v ] && weight != NO_EDGE && ( d[ u ] + weight < d[ v ] ) ) {
+                d[ v ] = d[ u ] + weight ;
             }
+            
         }
     }
 
-    static int res[ 4 ] ;
-    res[ 0 ] = d[ 1 ] ;
-    res[ 1 ] = d[ 2 ] ;
-    res[ 2 ] = d[ 3 ] ;
-    res[ 3 ] = d[ 4 ] ;
+    free( visited ) ;
+    
+    return d ;
+}
 
-    return res ;
+int main()
+{
+    int n = 5 ;
+    int i ;
+    int graph[] = {
+        0, 10, NO_EDGE, 30, 100,
+        10, 0, 50, NO_EDGE, NO_EDGE,
+        NO_EDGE, 50, 0, 20, 10,
+        30, NO_EDGE, 20, 0, 60,
+        100, NO_EDGE, 10, 60, 0
+    } ;
+
+    int *distances = Dijkstra( graph, n ) ;
+    
+    printf( "Vertex Distance from Source (0) :\n" ) ;
+    for ( i = 0 ; i < n ; i++ ) {
+        printf( "Vertex %d : %d\n", i, distances[ i ] ) ;
+    }
+
+    free( distances ) ;
+
+    return 0 ;
 }
